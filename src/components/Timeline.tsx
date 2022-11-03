a5d82dad477e882cdf03949799089addc9ea22f9
Victor Martinez <vicplusplus@proton.me>
2022-11-03 00:44:54 -0400

    uh

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import Action from "../simulation/actions/Action";
+
+
 export default function Timeline() {
+
+    // timeline contents are a list of Actions
+    const [actions, setActions] = useState<Action[]>([]);
+
     return (
         <div className="Timeline">
 
