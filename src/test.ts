bd28c1c30997e3a474b510ba1253cd58aebb1559
Victor Martinez <vicplusplus@proton.me>
2023-06-17 15:57:46 -0400

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,20 +1,7 @@
-import { BloonPack } from "./BloonPack";
 import { GameRules } from "./GameRules";
 import { simulate } from "./Simulation";
 import { BloonSendBreakpointAction } from "./actions/BloonSendBreakpointAction";
-import { QueueBloonPackAction } from "./actions/QueueBloonPackAction";
 import { readFileSync } from "fs"
-
-
-const bloonPacks: { [key: string]: BloonPack } = {
-    groupedReds: {
-        name: "groupedReds",
-        price: 25,
-        eco: 1.2,
-        roundsAvailable: [1, 9],
-        timeToSend: 0.8
-    }
-}
 
 // read rules from data/1.0.0.json
 const rules: GameRules = JSON.parse(readFileSync("data/1.0.0.json", "utf8"));
